be72b7d3bd71de9471d3a4f003603e7b8a81aa74
pldi17-17 <pldi2017undef@gmail.com>
2017-02-20 13:08:33 +0900

    Add checkout-lnt.sh

---
+++ checkout-lnt.sh
@@ -0,0 +1,5 @@
+mkdir lntsuite
+cd lntsuite
+svn checkout --revision r279179 http://llvm.org/svn/llvm-project/lnt/trunk
+svn checkout --revision r275311 http://llvm.org/svn/llvm-project/test-suite/trunk
+
